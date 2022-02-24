02f054f49c067ef01ff859f7abce1d8046a87018
fulajim <35881973@qq.com>
2022-02-24 16:09:59 +0800

    完成啦订单功能的开发

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -151,7 +151,7 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error("获取订单列表失败!");
       }
-      console.log(res);
+    //   console.log(res);
       this.total = res.data.total;
       this.orderlist = res.data.goods;
     },
@@ -178,7 +178,7 @@ export default {
       }
       this.progressInfo = res.data;
       this.progressVisible = true;
-      console.log(this.progressInfo);
+    //   console.log(this.progressInfo);
     },
   },
   components: {},
